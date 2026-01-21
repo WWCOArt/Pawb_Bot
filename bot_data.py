af5b4136b34adf17f901540f3aa6b758c4256718
Diane Sparks <duncan.sprks@gmail.com>
2026-01-20 18:20:23 -0800

    hype train + silly mode

--- bot_data.py
+++ bot_data.py
@@ -6,6 +6,8 @@ class BotData():
 		self.distracted_count = 0
 		self.silly_mode = False
 		self.avatar = "sphinx"
+		self.current_hype_level = 0
+		self.highest_hype_level = 0
 
 		with open("foxrules.json", encoding="utf8") as foxrules_file:
 			self.foxrules = json.load(foxrules_file)
