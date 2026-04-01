9e851066ede2abe84e7ab770c8ab30dacefd4684
Diane Sparks <duncan.sprks@gmail.com>
2026-03-31 20:23:08 -0700

    i am so good at this

--- utility_functions.py
+++ utility_functions.py
@@ -111,4 +111,4 @@ async def send_message(user: PartialUser, sender: str | int | PartialUser, messa
 	await user.send_message(sender=sender, message=string_to_leetspeak(message))
 
 async def send_message_context(context: commands.Context, message: str):
-	await context.send(message)
+	await context.send(string_to_leetspeak(message))
