6ee507c239bacd2f28079e4b5b8e55960088bfb4
sierra <librarysphinx047@gmail.com>
2026-08-15 09:13:24 -0500

    small update to iki gremlin command to hide guests

--- commands_misc.py
+++ commands_misc.py
@@ -80,7 +80,9 @@ class CommandsMisc(commands.Component):
 	async def headpats(self, context: commands.Context):
 		if context.author.name == "ikikoroboros":
 			subprocess.run(f'{self.bot.VEADOTUBE_PATH} -i 0 nodes stateEvents tempInteract set "IkiGremlin"')
+			subprocess.run(f'{self.bot.VEADOTUBE_PATH} -i 0 nodes stateEvents hideGuests set "on"')
 			await asyncio.sleep(2)
+			subprocess.run(f'{self.bot.VEADOTUBE_PATH} -i 0 nodes stateEvents hideGuests set "(off)"')
 			subprocess.run(f'{self.bot.VEADOTUBE_PATH} -i 0 nodes stateEvents tempInteract set "(off)"')
 
 	@commands.command()
